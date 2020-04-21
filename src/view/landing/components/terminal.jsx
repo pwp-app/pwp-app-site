a81b8920329512b215f83e74b34c1a226a23ceac
BackRunner <12205593+backrunner@users.noreply.github.com>
2020-04-22 01:10:41 +0800

    update: terminal anim wrapper

--- src/view/landing/components/terminal.jsx
+++ src/view/landing/components/terminal.jsx
@@ -1,6 +1,11 @@
 import React from 'react';
+import { OverPack } from 'rc-scroll-anim';
 
 class Terminal extends React.PureComponent{
+    onChange = ({mode, id}) => {
+        console.log(mode)
+        console.log(id);
+    }
     render() {
         return (
             <div className="terminal">
@@ -14,21 +19,23 @@ class Terminal extends React.PureComponent{
                         <span>terminal</span>
                     </div>
                 </div>
-                <div className="terminal-body">
-                    <p>> root@pwp.app:~# ./about</p>
-                    <p>> Starting about pwp.app...</p>
-                    <p>> Program started.</p>
-                    <p>> ==============================</p>
-                    <p>> pwp.app is a civil software development team, </p>
-                    <p>> team created in 2019, dedicated to turning great ideas into visible code and available tools.</p>
-                    <p>> We focus on front-end technology to explore the technological frontier, </p>
-                    <p>> contribute the results we get based on our ideas to the open source community.</p>
-                    <p>> We make programming meaningful, creative, and valuable, </p>
-                    <p>> we just do what we want.</p>
-                    <p>> ==============================</p>
-                    <p>> Exit code 0.</p>
-                    <p>> root@pwp.app:~# <span className="icon-input"></span></p>
-                </div>
+                <OverPack always={false} onChange={this.onChange}>
+                    <div className="terminal-body">
+                        <p>> root@pwp.app:~# ./about</p>
+                        <p>Starting about pwp.app...</p>
+                        <p>Program started.</p>
+                        <p>==============================</p>
+                        <p>pwp.app is a civil software development team, </p>
+                        <p>team created in 2019, dedicated to turning great ideas into visible code and available tools.</p>
+                        <p>We focus on front-end technology to explore the technological frontier, </p>
+                        <p>contribute the results we get based on our ideas to the open source community.</p>
+                        <p>We make programming meaningful, creative, and valuable, </p>
+                        <p>we just do what we want.</p>
+                        <p>==============================</p>
+                        <p>Exit code 0.</p>
+                        <p>> root@pwp.app:~# <span className="icon-input"></span></p>
+                    </div>
+                </OverPack>
             </div>
         );
     }
