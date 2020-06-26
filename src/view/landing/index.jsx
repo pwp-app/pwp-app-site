c3c67da05a1b219d030fcd67ef42a3b59a64563c
BackRunner <12205593+backrunner@users.noreply.github.com>
2020-06-27 02:32:45 +0800

    add: discuss section

--- src/view/landing/index.jsx
+++ src/view/landing/index.jsx
@@ -6,6 +6,7 @@ import About from './section/aboutus';
 import Projects from './section/projects';
 import Members from './section/members';
 import Thanks from './section/thanks';
+import Discuss from './section/discuss';
 
 class Landing extends React.PureComponent {
     render () {
@@ -16,6 +17,7 @@ class Landing extends React.PureComponent {
                 <About/>,
                 <Projects/>,
                 <Members/>,
+                <Discuss/>,
                 <Thanks/>,
                 <Footer/>
             ]
